8abbfab91602ae7c29382b3c32751b8b5580ec62
alopsantos <anderson_lops@hotmail.com>
2023-10-15 17:53:05 -0300

    add youtube

---
+++ youtubedownload.py
@@ -0,0 +1,5 @@
+from pytube import YouTube
+YouTube('https://www.youtube.com/watch?v=mh0rmYptH-0').streams.first().download()
+yt = YouTube('https://www.youtube.com/watch?v=mh0rmYptH-0')
+yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+    'resolution').desc().first().download()
