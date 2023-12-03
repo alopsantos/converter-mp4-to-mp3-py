8e55c5017e11caccab76b8d1508771a3fc446fbf
alopsantos <anderson_lops@hotmail.com>
2023-12-03 13:02:16 -0300

    :hammer: Update code in name and extensão auto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import moviepy.editor
+import re
 from pathlib import Path
 
 
@@ -7,5 +8,6 @@ path_list = Path(directory_to_save).glob("*.mp4")
 
 for arquivo in path_list:
     print(arquivo)
+    nome = str(arquivo).split('audios/')[1].split('.mp4')[0]
     videoclip = moviepy.editor.VideoFileClip(str(arquivo))
-    videoclip.audio.write_audiofile(f"{str(arquivo)}.mp3")
+    videoclip.audio.write_audiofile(f"audios/{nome}.mp3")
--- youtubedownload.py
+++ youtubedownload.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-YouTube('https://www.youtube.com/watch?v=mh0rmYptH-0').streams.first().download()
-yt = YouTube('https://www.youtube.com/watch?v=mh0rmYptH-0')
+YouTube('https://www.youtube.com/watch?v=RhmDyjOhkD4').streams.first().download()
+yt = YouTube('https://www.youtube.com/watch?v=RhmDyjOhkD4')
 yt.streams.filter(progressive=True, file_extension='mp4').order_by(
     'resolution').desc().first().download()
