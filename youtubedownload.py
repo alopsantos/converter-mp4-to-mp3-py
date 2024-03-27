7c8395fae34126081cc928f0212f726c708e95d3
alopsantos <anderson_lops@hotmail.com>
2024-03-27 15:37:06 -0300

    :hammer: Download for list

--- youtubedownload.py
+++ youtubedownload.py
@@ -1,5 +1,18 @@
 from pytube import YouTube
-YouTube('https://www.youtube.com/watch?v=RhmDyjOhkD4').streams.first().download()
-yt = YouTube('https://www.youtube.com/watch?v=RhmDyjOhkD4')
-yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-    'resolution').desc().first().download()
+
+
+arquivo = open(
+    "youtube.txt")
+produtos = arquivo.readlines()
+arquivo.close()
+
+
+for index in range(len(produtos)):
+    produtos[index] = produtos[index].rstrip('\n')
+    produto = produtos[index]
+    print(produto)
+
+    YouTube(produto).streams.first().download()
+    yt = YouTube(produto)
+    yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first().download()
